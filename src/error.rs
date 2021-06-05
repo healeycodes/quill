54b57a623c158bf406010e2ea3bf3083a03cb88e
Andrew <healeycodes@gmail.com>
2021-06-05 06:53:45 +0100

    Add lexer

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,13 @@
+// Error reasons are enumerated here to be used in the Err struct,
+// the error type shared across all Ink APIs.
 pub const ERR_UNKNOWN: i32 = 0;
 pub const ERR_SYNTAX: i32 = 1;
 pub const ERR_RUNTIME: i32 = 2;
 pub const ERR_SYSTEM: i32 = 40;
 pub const ERR_ASSERT: i32 = 100;
 
+// Err constants represent possible errors that Ink interpreter
+// binding functions may return.
 #[derive(Debug)]
 pub struct Err {
     pub reason: i32,
---
+++ src/lexer.rs
@@ -0,0 +1,2 @@
+type Kind i32;
+
